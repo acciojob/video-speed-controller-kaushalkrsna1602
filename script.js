af059f00f099c4b2db6118586c330b617e0d9800
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-12-09 23:14:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-const inputs = document.querySelectorAll('.controls input');
-const player = document.querySelector(".player");
-const video = player.querySelector(".viewer");
-const progress = player.querySelector(".progress");
-const progressBar = player.querySelector(".progress__filled");
-const toggle = player.querySelector(".toggle");
-const skipButtons = player.querySelectorAll("[data-skip]");
-const ranges = player.querySelectorAll(".player__slider");
+// const inputs = document.querySelectorAll('.controls input');
+// const player = document.querySelector(".player");
+// const video = player.querySelector(".viewer");
+// const progress = player.querySelector(".progress");
+// const progressBar = player.querySelector(".progress__filled");
+// const toggle = player.querySelector(".toggle");
+// const skipButtons = player.querySelectorAll("[data-skip]");
+// const ranges = player.querySelectorAll(".player__slider");
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
-toggle.addEventListener("click", togglePlay);
+//     function handleUpdate() {
+//       const suffix = this.dataset.sizing || '';
+//       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+//     }
+// toggle.addEventListener("click", togglePlay);
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
-video.addEventListener("timeupdate", handlerProgress);
+//     inputs.forEach(input => input.addEventListener('change', handleUpdate));
+//     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// video.addEventListener("timeupdate", handlerProgress);
 
-for (let skip of skipButtons) {
-  skip.addEventListener("click", forwardOrBackward);
-}
+// for (let skip of skipButtons) {
+//   skip.addEventListener("click", forwardOrBackward);
+// }
 
-for (let range of ranges) {
-  range.addEventListener("change", handleRangeUpdate);
-}
+// for (let range of ranges) {
+//   range.addEventListener("change", handleRangeUpdate);
+// }
 
-function togglePlay() {
-  if (video.paused) {
-    video.play();
-    toggle.innerText = "❚ ❚";
-  } else {
-    video.pause();
-    toggle.innerText = "►";
-  }
-}
+// function togglePlay() {
+//   if (video.paused) {
+//     video.play();
+//     toggle.innerText = "❚ ❚";
+//   } else {
+//     video.pause();
+//     toggle.innerText = "►";
+//   }
+// }
 
-function handlerProgress() {
-  const currentProgress = (video.currentTime / video.duration) * 100;
-  progressBar.style.flexBasis = `${currentProgress}%`;
-}
+// function handlerProgress() {
+//   const currentProgress = (video.currentTime / video.duration) * 100;
+//   progressBar.style.flexBasis = `${currentProgress}%`;
+// }
 
-function forwardOrBackward(event) {
-  let element = event.target;
-  video.currentTime += parseFloat(element.attributes["data-skip"].value);
-}
+// function forwardOrBackward(event) {
+//   let element = event.target;
+//   video.currentTime += parseFloat(element.attributes["data-skip"].value);
+// }
 
-function handleRangeUpdate(event) {
-  let element = event.target;
-  video[element.name] = element.value;
-}
+// function handleRangeUpdate(event) {
+//   let element = event.target;
+//   video[element.name] = element.value;
+// }
 
-const rewindButton = player.querySelector(".rewind");
+// const rewindButton = player.querySelector(".rewind");
 
-rewindButton.addEventListener("click", rewindVideo);
+// rewindButton.addEventListener("click", rewindVideo);
 
-function rewindVideo() {
-  video.currentTime = 0; // Rewind to the beginning
-}
+// function rewindVideo() {
+//   video.currentTime = 0; 
+// }
